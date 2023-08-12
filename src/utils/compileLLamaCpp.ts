ab3d10e3fde98adb6d573ad2d2ef1df0a470d576
Gilad S <giladgd@gmail.com>
2023-08-13 01:07:10 +0300

    feat: use `spawn` directly instead of `$` to avoid redundant logs

--- src/utils/compileLLamaCpp.ts
+++ src/utils/compileLLamaCpp.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import {fileURLToPath} from "url";
-import {$} from "zx";
+import process from "process";
 import fs from "fs-extra";
 import {llamaCppDirectory, llamaDirectory} from "../config.js";
 import {clearLlamaBuild} from "./clearLlamaBuild.js";
 import {setUsedBinFlag} from "./usedBinFlag.js";
+import {spawnCommand} from "./spawnCommand.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -18,9 +19,9 @@ export async function compileLlamaCpp({arch = process.arch, nodeTarget = process
 
         await clearLlamaBuild();
 
-        await $`npm run -s node-gyp-llama -- configure --arch=${arch} --target=${nodeTarget}`;
+        await spawnCommand("npm", ["run", "-s", "node-gyp-llama", "--", "configure", "--arch=" + arch, "--target=" + nodeTarget], __dirname);
 
-        await $`npm run -s node-gyp-llama -- configure --arch=${arch} --target=${nodeTarget} -- -f compile_commands_json`;
+        await spawnCommand("npm", ["run", "-s", "node-gyp-llama", "--", "configure", "--arch=" + arch, "--target=" + nodeTarget, "--", "-f", "compile_commands_json"], __dirname);
 
         if (await fs.exists(path.join(llamaDirectory, "Release", "compile_commands.json"))) {
             await fs.move(
@@ -38,7 +39,7 @@ export async function compileLlamaCpp({arch = process.arch, nodeTarget = process
         await fs.remove(path.join(llamaDirectory, "Debug"));
 
 
-        await $`npm run -s node-gyp-llama-build -- --arch=${arch} --target=${nodeTarget}`;
+        await spawnCommand("npm", ["run", "-s", "node-gyp-llama-build", "--", "--arch=" + arch, "--target=" + nodeTarget], __dirname);
 
         if (setUsedBingFlag) {
             await setUsedBinFlag("localBuildFromSource");
---
+++ src/utils/spawnCommand.ts
@@ -0,0 +1,42 @@
+import {spawn} from "child_process";
+
+export function spawnCommand(command: string, args: string[], cwd: string) {
+    function getCommandString() {
+        let res = command;
+
+        for (const arg of args) {
+            if (arg.includes(" ")) {
+                res += ` "${arg.split('"').join('\\"')}"`;
+            } else {
+                res += ` ${arg}`;
+            }
+        }
+
+        return res;
+    }
+
+    return new Promise<void>((resolve, reject) => {
+        const child = spawn(command, args, {
+            stdio: "inherit",
+            cwd,
+            env: process.env,
+            detached: false,
+            windowsHide: true
+        });
+
+        child.on("exit", (code) => {
+            if (code == 0)
+                resolve();
+            else
+                reject(new Error(`Command ${getCommandString()} exited with code ${code}`));
+        });
+        child.on("error", reject);
+        child.on("disconnect", () => reject(new Error(`Command ${getCommandString()} disconnected`)));
+        child.on("close", code => {
+            if (code == 0)
+                resolve();
+            else
+                reject(new Error(`Command ${getCommandString()} closed with code ${code}`));
+        });
+    });
+}
