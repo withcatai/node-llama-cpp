ed4debaadf395556771f68b080f391cc63c539ea
Ido S <50456099+ido-pluto@users.noreply.github.com>
2023-08-16 21:50:31 +0300

    Fix/chat context (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import {LlamaChatSession} from "./LlamaChatSession.js";
-import {LlamaModel} from "./LlamaModel.js";
+import {LlamaChatSession} from "./llamaEvaluator/LlamaChatSession.js";
+import {LlamaModel} from "./llamaEvaluator/LlamaModel.js";
 import {AbortError} from "./AbortError.js";
 import {ChatPromptWrapper} from "./ChatPromptWrapper.js";
 import {EmptyChatPromptWrapper} from "./chatWrappers/EmptyChatPromptWrapper.js";
 import {LlamaChatPromptWrapper} from "./chatWrappers/LlamaChatPromptWrapper.js";
 import {GeneralChatPromptWrapper} from "./chatWrappers/GeneralChatPromptWrapper.js";
+import {LlamaContext} from "./llamaEvaluator/LlamaContext.js";
+
 
 export {
     LlamaModel,
+    LlamaContext,
     LlamaChatSession,
     AbortError,
     ChatPromptWrapper,
