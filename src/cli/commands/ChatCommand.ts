8cbebcfdb86cf831e0aae38a4d693103c2486213
Gilad S <giladgd@gmail.com>
2023-08-28 00:56:09 +0300

    feat: add `maxTokens` support to chat command and aliases

--- src/cli/commands/ChatCommand.ts
+++ src/cli/commands/ChatCommand.ts
@@ -18,7 +18,8 @@ type ChatCommand = {
     grammar: "text" | Parameters<typeof LlamaGrammar.getFor>[0],
     temperature: number,
     topK: number,
-    topP: number
+    topP: number,
+    maxTokens: number
 };
 
 export const ChatCommand: CommandModule<object, ChatCommand> = {
@@ -27,18 +28,21 @@ export const ChatCommand: CommandModule<object, ChatCommand> = {
     builder(yargs) {
         return yargs
             .option("model", {
+                alias: "m",
                 type: "string",
                 demandOption: true,
                 description: "LLama model file to use for the chat",
                 group: "Required:"
             })
             .option("systemInfo", {
+                alias: "i",
                 type: "boolean",
                 default: false,
                 description: "Print llama.cpp system info",
                 group: "Optional:"
             })
             .option("systemPrompt", {
+                alias: "s",
                 type: "string",
                 default: defaultChatSystemPrompt,
                 defaultDescription: "",
@@ -48,6 +52,7 @@ export const ChatCommand: CommandModule<object, ChatCommand> = {
                 group: "Optional:"
             })
             .option("wrapper", {
+                alias: "w",
                 type: "string",
                 default: "general" as ChatCommand["wrapper"],
                 choices: ["general", "llama", "chatML"] satisfies ChatCommand["wrapper"][],
@@ -55,12 +60,14 @@ export const ChatCommand: CommandModule<object, ChatCommand> = {
                 group: "Optional:"
             })
             .option("contextSize", {
+                alias: "c",
                 type: "number",
                 default: 1024 * 4,
                 description: "Context size to use for the model",
                 group: "Optional:"
             })
             .option("grammar", {
+                alias: "g",
                 type: "string",
                 default: "text" as ChatCommand["grammar"],
                 choices: ["text", "json", "list", "arithmetic", "japanese", "chess"] satisfies ChatCommand["grammar"][],
@@ -68,30 +75,40 @@ export const ChatCommand: CommandModule<object, ChatCommand> = {
                 group: "Optional:"
             })
             .option("temperature", {
+                alias: "t",
                 type: "number",
                 default: 0,
                 description: "Temperature is a hyperparameter that controls the randomness of the generated text. It affects the probability distribution of the model's output tokens. A higher temperature (e.g., 1.5) makes the output more random and creative, while a lower temperature (e.g., 0.5) makes the output more focused, deterministic, and conservative. The suggested temperature is 0.8, which provides a balance between randomness and determinism. At the extreme, a temperature of 0 will always pick the most likely next token, leading to identical outputs in each run. Set to `0` to disable.",
                 group: "Optional:"
             })
             .option("topK", {
+                alias: "k",
                 type: "number",
                 default: 40,
                 description: "Limits the model to consider only the K most likely next tokens for sampling at each step of sequence generation. An integer number between `1` and the size of the vocabulary. Set to `0` to disable (which uses the full vocabulary). Only relevant when `temperature` is set to a value greater than 0.",
                 group: "Optional:"
             })
             .option("topP", {
+                alias: "p",
                 type: "number",
                 default: 0.95,
                 description: "Dynamically selects the smallest set of tokens whose cumulative probability exceeds the threshold P, and samples the next token only from this set. A float number between `0` and `1`. Set to `1` to disable. Only relevant when `temperature` is set to a value greater than `0`.",
+                group: "Optional:"
+            })
+            .option("maxTokens", {
+                alias: "mt",
+                type: "number",
+                default: 0,
+                description: "Maximum number of tokens to generate in responses. Set to `0` to disable. Set to `-1` to set to the context size",
                 group: "Optional:"
             });
     },
     async handler({
         model, systemInfo, systemPrompt, wrapper, contextSize, grammar,
-        temperature, topK, topP
+        temperature, topK, topP, maxTokens
     }) {
         try {
-            await RunChat({model, systemInfo, systemPrompt, wrapper, contextSize, grammar, temperature, topK, topP});
+            await RunChat({model, systemInfo, systemPrompt, wrapper, contextSize, grammar, temperature, topK, topP, maxTokens});
         } catch (err) {
             console.error(err);
             process.exit(1);
@@ -101,7 +118,7 @@ export const ChatCommand: CommandModule<object, ChatCommand> = {
 
 
 async function RunChat({
-    model: modelArg, systemInfo, systemPrompt, wrapper, contextSize, grammar: grammarArg, temperature, topK, topP
+    model: modelArg, systemInfo, systemPrompt, wrapper, contextSize, grammar: grammarArg, temperature, topK, topP, maxTokens
 }: ChatCommand) {
     const {LlamaChatSession} = await import("../../llamaEvaluator/LlamaChatSession.js");
     const {LlamaModel} = await import("../../llamaEvaluator/LlamaModel.js");
@@ -156,8 +173,15 @@ async function RunChat({
         const [startColor, endColor] = chalk.blue("MIDDLE").split("MIDDLE");
 
         process.stdout.write(startColor);
-        await session.prompt(input, (chunk) => {
-            process.stdout.write(session.context.decode(Uint32Array.from(chunk)));
+        await session.prompt(input, {
+            maxTokens: maxTokens === -1
+                ? context.getContextSize()
+                : maxTokens <= 0
+                    ? undefined
+                    : maxTokens,
+            onToken(chunk) {
+                process.stdout.write(session.context.decode(Uint32Array.from(chunk)));
+            }
         });
         process.stdout.write(endColor);
         console.log();
