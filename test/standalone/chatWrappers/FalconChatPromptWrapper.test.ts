1486f49c64f110cb8484c28c70b008875a41760a
Gilad S <giladgd@gmail.com>
2024-01-18 01:11:01 +0200

    test: update tests

--- test/standalone/chatWrappers/FalconChatPromptWrapper.test.ts
+++ test/standalone/chatWrappers/FalconChatPromptWrapper.test.ts
@@ -1,140 +1,153 @@
 import {describe, expect, test} from "vitest";
-import {generateContextTextFromConversationHistory} from "../../../src/chatWrappers/generateContextTextFromConversationHistory.js";
-import {ConversationInteraction, FalconChatPromptWrapper} from "../../../src/index.js";
+import {ChatHistoryItem, FalconChatWrapper} from "../../../src/index.js";
+import {defaultChatSystemPrompt} from "../../../src/config.js";
 
 
-describe("FalconChatPromptWrapper", () => {
-    const conversationHistory: ConversationInteraction[] = [{
-        prompt: "Hi there!",
-        response: "Hello!"
-    }];
-    const conversationHistory2: ConversationInteraction[] = [{
-        prompt: "Hi there!",
-        response: "Hello!"
+describe("FalconChatWrapper", () => {
+    const conversationHistory: ChatHistoryItem[] = [{
+        type: "system",
+        text: defaultChatSystemPrompt
     }, {
-        prompt: "How are you?",
-        response: "I'm good, how are you?"
+        type: "user",
+        text: "Hi there!"
+    }, {
+        type: "model",
+        response: ["Hello!"]
+    }];
+    const conversationHistory2: ChatHistoryItem[] = [{
+        type: "system",
+        text: defaultChatSystemPrompt
+    }, {
+        type: "user",
+        text: "Hi there!"
+    }, {
+        type: "model",
+        response: ["Hello!"]
+    }, {
+        type: "user",
+        text: "How are you?"
+    }, {
+        type: "model",
+        response: ["I'm good, how are you?"]
     }];
 
-    test("should generate valid output for default roles", () => {
-        const chatWrapper = new FalconChatPromptWrapper();
-        const {text: response} = generateContextTextFromConversationHistory(chatWrapper, conversationHistory);
+    test("should generate valid context text", () => {
+        const chatWrapper = new FalconChatWrapper();
+        const {contextText} = chatWrapper.generateContextText(conversationHistory);
 
-        expect(response).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          User: Hi there!
-          Assistant: Hello!
-          User: "
+        expect(contextText.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            "
+
+          ",
+            "User: ",
+            "Hi there!",
+            "
+
+          ",
+            "Assistant: ",
+            "Hello!",
+          ]
         `);
 
-        const chatWrapper2 = new FalconChatPromptWrapper();
-        const {text: response2} = generateContextTextFromConversationHistory(chatWrapper2, conversationHistory2);
+        const chatWrapper2 = new FalconChatWrapper();
+        const {contextText: contextText2} = chatWrapper2.generateContextText(conversationHistory2);
 
-        expect(response2).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          User: Hi there!
-          Assistant: Hello!
-          User: How are you?
-          Assistant: I'm good, how are you?
-          User: "
+        expect(contextText2.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            "
+
+          ",
+            "User: ",
+            "Hi there!",
+            "
+
+          ",
+            "Assistant: ",
+            "Hello!",
+            "
+
+          ",
+            "User: ",
+            "How are you?",
+            "
+
+          ",
+            "Assistant: ",
+            "I'm good, how are you?",
+          ]
         `);
 
-        const chatWrapper3 = new FalconChatPromptWrapper();
-        const {text: response3, stopStringSuffix, stopString} = generateContextTextFromConversationHistory(chatWrapper3, conversationHistory);
+        const chatWrapper3 = new FalconChatWrapper();
+        const {contextText: contextText3} = chatWrapper3.generateContextText(conversationHistory);
+        const {contextText: contextText3WithOpenModelResponse} = chatWrapper3.generateContextText([
+            ...conversationHistory,
+            {
+                type: "model",
+                response: []
+            }
+        ]);
 
-        const newPrompt = conversationHistory2[1].prompt;
-        const wrappedNewPrompt = chatWrapper3.wrapPrompt(newPrompt, {
-            systemPrompt: response3,
-            promptIndex: 1,
-            lastStopString: stopString,
-            lastStopStringSuffix: stopStringSuffix
-        });
+        expect(contextText3.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            "
 
-        expect(response3).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          User: Hi there!
-          Assistant: Hello!
-          User: "
+          ",
+            "User: ",
+            "Hi there!",
+            "
+
+          ",
+            "Assistant: ",
+            "Hello!",
+          ]
         `);
 
-        expect(wrappedNewPrompt).toMatchInlineSnapshot(`
-          "How are you?
-          Assistant: "
-        `);
+        expect(contextText3WithOpenModelResponse.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            "
 
-        expect(response3 + wrappedNewPrompt).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          User: Hi there!
-          Assistant: Hello!
-          User: How are you?
-          Assistant: "
-        `);
-    });
+          ",
+            "User: ",
+            "Hi there!",
+            "
 
-    test("should generate valid output for custom roles", () => {
-        const chatWrapper = new FalconChatPromptWrapper({
-            instructionName: "Instruction",
-            responseName: "Response"
-        });
-        const {text: response} = generateContextTextFromConversationHistory(chatWrapper, conversationHistory);
+          ",
+            "Assistant: ",
+            "Hello!",
+            "
 
-        expect(response).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          Instruction: Hi there!
-          Response: Hello!
-          Instruction: "
-        `);
-
-        const chatWrapper2 = new FalconChatPromptWrapper({
-            instructionName: "Instruction",
-            responseName: "Response"
-        });
-        const {text: response2} = generateContextTextFromConversationHistory(chatWrapper2, conversationHistory2);
-
-        expect(response2).toMatchInlineSnapshot(`
-          "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-          Instruction: Hi there!
-          Response: Hello!
-          Instruction: How are you?
-          Response: I'm good, how are you?
-          Instruction: "
-        `);
-    });
-
-    test("should generate valid output for custom system prompt", () => {
-        const chatWrapper = new FalconChatPromptWrapper();
-        const {text: response} = generateContextTextFromConversationHistory(chatWrapper, conversationHistory, {
-            systemPrompt: "Below is an instruction the describes a task, Write a response the appropriately completes the request."
-        });
-
-        expect(response).toMatchInlineSnapshot(`
-          "Below is an instruction the describes a task, Write a response the appropriately completes the request.
-          User: Hi there!
-          Assistant: Hello!
-          User: "
-        `);
-
-        const chatWrapper2 = new FalconChatPromptWrapper({
-            instructionName: "Instruction",
-            responseName: "Response"
-        });
-        const {text: response2} = generateContextTextFromConversationHistory(chatWrapper2, conversationHistory2, {
-            systemPrompt: "Below is an instruction the describes a task, Write a response the appropriately completes the request."
-        });
-
-        expect(response2).toMatchInlineSnapshot(`
-          "Below is an instruction the describes a task, Write a response the appropriately completes the request.
-          Instruction: Hi there!
-          Response: Hello!
-          Instruction: How are you?
-          Response: I'm good, how are you?
-          Instruction: "
+          ",
+            "Assistant: ",
+          ]
         `);
     });
 });
--- test/standalone/chatWrappers/LlamaChatPromptWrapper.test.ts
+++ test/standalone/chatWrappers/LlamaChatPromptWrapper.test.ts
@@ -1,125 +1,219 @@
 import {describe, expect, test} from "vitest";
-import {generateContextTextFromConversationHistory} from "../../../src/chatWrappers/generateContextTextFromConversationHistory.js";
-import {ConversationInteraction, LlamaChatPromptWrapper} from "../../../src/index.js";
+import {ChatHistoryItem, LlamaChatWrapper} from "../../../src/index.js";
+import {defaultChatSystemPrompt} from "../../../src/config.js";
 
 
-describe("LlamaChatPromptWrapper", () => {
-    const conversationHistory: ConversationInteraction[] = [{
-        prompt: "Hi there!",
-        response: "Hello!"
-    }];
-    const conversationHistory2: ConversationInteraction[] = [{
-        prompt: "Hi there!",
-        response: "Hello!"
+describe("LlamaChatWrapper", () => {
+    const conversationHistory: ChatHistoryItem[] = [{
+        type: "system",
+        text: defaultChatSystemPrompt
     }, {
-        prompt: "How are you?",
-        response: "I'm good, how are you?"
+        type: "user",
+        text: "Hi there!"
+    }, {
+        type: "model",
+        response: ["Hello!"]
+    }];
+    const conversationHistory2: ChatHistoryItem[] = [{
+        type: "system",
+        text: defaultChatSystemPrompt
+    }, {
+        type: "user",
+        text: "Hi there!"
+    }, {
+        type: "model",
+        response: ["Hello!"]
+    }, {
+        type: "user",
+        text: "How are you?"
+    }, {
+        type: "model",
+        response: ["I'm good, how are you?"]
     }];
 
-    test("should generate valid output for default roles", () => {
-        const chatWrapper = new LlamaChatPromptWrapper();
-        const {text: response} = generateContextTextFromConversationHistory(chatWrapper, conversationHistory);
+    test("should generate valid context text", () => {
+        const chatWrapper = new LlamaChatWrapper();
+        const {contextText} = chatWrapper.generateContextText(conversationHistory);
 
-        expect(response).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+        expect(contextText.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            {
+              "type": "specialToken",
+              "value": "[INST] ",
+            },
+            {
+              "type": "specialToken",
+              "value": "<<SYS>>
+          ",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            {
+              "type": "specialToken",
+              "value": "
           <</SYS>>
 
-          Hi there! [/INST]
+          ",
+            },
+            "Hi there!",
+            {
+              "type": "specialToken",
+              "value": " [/INST]
 
-          Hello!</s>"
+          ",
+            },
+            "Hello!",
+          ]
         `);
 
-        const chatWrapper2 = new LlamaChatPromptWrapper();
-        const {text: response2} = generateContextTextFromConversationHistory(chatWrapper2, conversationHistory2);
+        const chatWrapper2 = new LlamaChatWrapper();
+        const {contextText: contextText2} = chatWrapper2.generateContextText(conversationHistory2);
 
-        expect(response2).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+        expect(contextText2.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            {
+              "type": "specialToken",
+              "value": "[INST] ",
+            },
+            {
+              "type": "specialToken",
+              "value": "<<SYS>>
+          ",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            {
+              "type": "specialToken",
+              "value": "
           <</SYS>>
 
-          Hi there! [/INST]
+          ",
+            },
+            "Hi there!",
+            {
+              "type": "specialToken",
+              "value": " [/INST]
 
-          Hello!</s><s>[INST] How are you? [/INST]
+          ",
+            },
+            "Hello!",
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "EOS",
+            },
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            {
+              "type": "specialToken",
+              "value": "[INST] ",
+            },
+            "How are you?",
+            {
+              "type": "specialToken",
+              "value": " [/INST]
 
-          I'm good, how are you?</s>"
+          ",
+            },
+            "I'm good, how are you?",
+          ]
         `);
 
-        const chatWrapper3 = new LlamaChatPromptWrapper();
-        const {text: response3, stopStringSuffix, stopString} = generateContextTextFromConversationHistory(chatWrapper3, conversationHistory);
+        const chatWrapper3 = new LlamaChatWrapper();
+        const {contextText: contextText3} = chatWrapper3.generateContextText(conversationHistory);
+        const {contextText: contextText3WithOpenModelResponse} = chatWrapper3.generateContextText([
+            ...conversationHistory,
+            {
+                type: "model",
+                response: []
+            }
+        ]);
 
-        const newPrompt = conversationHistory2[1].prompt;
-        const wrappedNewPrompt = chatWrapper3.wrapPrompt(newPrompt, {
-            systemPrompt: response3,
-            promptIndex: 1,
-            lastStopString: stopString,
-            lastStopStringSuffix: stopStringSuffix
-        });
-
-        expect(response3).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+        expect(contextText3.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            {
+              "type": "specialToken",
+              "value": "[INST] ",
+            },
+            {
+              "type": "specialToken",
+              "value": "<<SYS>>
+          ",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            {
+              "type": "specialToken",
+              "value": "
           <</SYS>>
 
-          Hi there! [/INST]
+          ",
+            },
+            "Hi there!",
+            {
+              "type": "specialToken",
+              "value": " [/INST]
 
-          Hello!</s>"
+          ",
+            },
+            "Hello!",
+          ]
         `);
 
-        expect(wrappedNewPrompt).toMatchInlineSnapshot(`
-          "<s>[INST] How are you? [/INST]
-
-          "
-        `);
-
-        expect(response3 + wrappedNewPrompt).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+        expect(contextText3WithOpenModelResponse.values).toMatchInlineSnapshot(`
+          [
+            {
+              "builtin": true,
+              "type": "specialToken",
+              "value": "BOS",
+            },
+            {
+              "type": "specialToken",
+              "value": "[INST] ",
+            },
+            {
+              "type": "specialToken",
+              "value": "<<SYS>>
+          ",
+            },
+            "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
+          If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
+            {
+              "type": "specialToken",
+              "value": "
           <</SYS>>
 
-          Hi there! [/INST]
+          ",
+            },
+            "Hi there!",
+            {
+              "type": "specialToken",
+              "value": " [/INST]
 
-          Hello!</s><s>[INST] How are you? [/INST]
+          ",
+            },
+            "Hello!
 
-          "
-        `);
-    });
-
-    test("should generate valid output for custom system prompt", () => {
-        const chatWrapper = new LlamaChatPromptWrapper();
-        const {text: response} = generateContextTextFromConversationHistory(chatWrapper, conversationHistory, {
-            systemPrompt: "Below is an instruction the describes a task, Write a response the appropriately completes the request."
-        });
-
-        expect(response).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          Below is an instruction the describes a task, Write a response the appropriately completes the request.
-          <</SYS>>
-
-          Hi there! [/INST]
-
-          Hello!</s>"
-        `);
-
-        const chatWrapper2 = new LlamaChatPromptWrapper();
-        const {text: response2} = generateContextTextFromConversationHistory(chatWrapper2, conversationHistory2, {
-            systemPrompt: "Below is an instruction the describes a task, Write a response the appropriately completes the request."
-        });
-
-        expect(response2).toMatchInlineSnapshot(`
-          "<s>[INST] <<SYS>>
-          Below is an instruction the describes a task, Write a response the appropriately completes the request.
-          <</SYS>>
-
-          Hi there! [/INST]
-
-          Hello!</s><s>[INST] How are you? [/INST]
-
-          I'm good, how are you?</s>"
+          ",
+          ]
         `);
     });
 });
