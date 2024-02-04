cc78961c2bb8d690640b1696181a1f95619afdf9
Gilad S <giladgd@users.noreply.github.com>
2024-02-05 00:23:46 +0200

    fix: release config (#154)

--- .releaserc.ts
+++ .releaserc.ts
@@ -1,21 +1,20 @@
 import {createRequire} from "module";
 import fs from "fs-extra";
-import {getReleaseInfo} from "./dist/utils/getReleaseInfo.js";
+import {getBinariesGithubRelease} from "./dist/bindings/utils/binariesGithubRelease.js";
 import {cliBinName, defaultLlamaCppGitHubRepo} from "./dist/config.js";
 
 const require = createRequire(import.meta.url);
 
 const defaultFooterTemplatePath = require.resolve("conventional-changelog-writer/templates/footer.hbs");
 const defaultFooterTemplate = await fs.readFile(defaultFooterTemplatePath, "utf8");
-const releaseInfo = await getReleaseInfo();
-const llamaCppRelease = releaseInfo.llamaCpp.release;
+const binariesSourceRelease = await getBinariesGithubRelease();
 const homepageUrl = require("./package.json").homepage;
 const homepageUrlWithoutTrailingSlash = homepageUrl.endsWith("/")
     ? homepageUrl.slice(0, -1)
     : homepageUrl;
 
 const newFooterTemplate = defaultFooterTemplate + "\n---\n\n" +
-    `Shipped with \`llama.cpp\` release [\`${llamaCppRelease.split("`").join("")}\`](https://github.com/${defaultLlamaCppGitHubRepo}/releases/tag/${encodeURIComponent(llamaCppRelease)})\n\n` +
+    `Shipped with \`llama.cpp\` release [\`${binariesSourceRelease.split("`").join("")}\`](https://github.com/${defaultLlamaCppGitHubRepo}/releases/tag/${encodeURIComponent(binariesSourceRelease)})\n\n` +
     `> To use the latest \`llama.cpp\` release available, run \`npx --no ${cliBinName} download --release latest\`. ([learn more](${homepageUrlWithoutTrailingSlash}/guide/building-from-source#downloading-a-newer-release))\n`;
 
 /**
