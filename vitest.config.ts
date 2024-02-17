2b359e9e7d76555f34200fe561202c3d38ac0946
Gilad S <giladgd@gmail.com>
2024-02-18 00:01:22 +0200

    fix: `vitest.config.ts`

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,9 @@ import {defineConfig} from "vitest/config";
 
 export default defineConfig({
     test: {
-        pool: "typescript",
+        pool: "threads",
+        maxWorkers: 1,
+        minWorkers: 1,
         poolOptions: {
             threads: {
                 minThreads: 1,
