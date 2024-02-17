b875e1a728a741b2462864edb4640ac945c237ef
Gilad S <giladgd@gmail.com>
2024-02-18 01:38:54 +0200

    fix: `vitest.config.ts`

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from "vitest/config";
 
 export default defineConfig({
     test: {
-        pool: "threads",
+        pool: "forks",
         maxWorkers: 1,
         minWorkers: 1,
         poolOptions: {
