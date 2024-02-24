bab2034fd4be60827adb204a800af28d492a4b1b
Gilad S <giladgd@gmail.com>
2024-02-24 21:37:16 +0200

    test: improve function calling tests

--- test/modelDependent/functionary/functions.test.ts
+++ test/modelDependent/functionary/functions.test.ts
@@ -1,11 +1,11 @@
 import {describe, expect, test} from "vitest";
-import {defineChatSessionFunction, LlamaChatSession, LlamaContext, LlamaModel} from "../../../src/index.js";
+import {defineChatSessionFunction, LlamaChatSession, LlamaContext, LlamaJsonSchemaGrammar, LlamaModel} from "../../../src/index.js";
 import {getModelFile} from "../../utils/modelFiles.js";
 import {getTestLlama} from "../../utils/getTestLlama.js";
 
 describe("functionary", () => {
     describe("functions", () => {
-        test("get time", async () => {
+        test("get n-th word", async () => {
             const modelPath = await getModelFile("functionary-small-v2.2.q4_0.gguf");
             const llama = await getTestLlama();
 
@@ -41,6 +41,65 @@ describe("functionary", () => {
             });
 
             expect(res).to.be.eq('The second word is "secret".');
+        }, {
+            timeout: 1000 * 60 * 60 * 2
+        });
+    });
+
+    describe("functions and grammar", () => {
+        test("get n-th word", async () => {
+            const modelPath = await getModelFile("functionary-small-v2.2.q4_0.gguf");
+            const llama = await getTestLlama();
+
+            const model = new LlamaModel({
+                llama,
+                modelPath
+            });
+            const context = new LlamaContext({
+                model,
+                contextSize: 4096
+            });
+            const chatSession = new LlamaChatSession({
+                contextSequence: context.getSequence()
+            });
+
+            const res = await chatSession.prompt("What is the second word?", {
+                functions: {
+                    getNthWord: defineChatSessionFunction({
+                        description: "Get an n-th word",
+                        params: {
+                            type: "object",
+                            properties: {
+                                n: {
+                                    enum: [1, 2, 3, 4]
+                                }
+                            }
+                        },
+                        handler(params) {
+                            return ["very", "secret", "this", "hello"][params.n - 1];
+                        }
+                    })
+                }
+            });
+
+            expect(res).to.be.eq('The second word is "secret".');
+
+            const res2SchemaGrammar = new LlamaJsonSchemaGrammar(llama, {
+                type: "object",
+                properties: {
+                    word: {
+                        type: "string"
+                    }
+                }
+            });
+
+            const res2 = await chatSession.prompt("Repeat your response", {
+                grammar: res2SchemaGrammar
+            });
+
+            const parsedRes2 = res2SchemaGrammar.parse(res2);
+
+            expect(parsedRes2).to.eql({word: "secret"});
         }, {
             timeout: 1000 * 60 * 60 * 2
         });
