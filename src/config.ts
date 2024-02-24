ffaaaa38a136ab82e20c6badc26819198360eb83
Gilad S <giladgd@gmail.com>
2024-02-24 21:36:27 +0200

    fix: default system prompt

--- src/config.ts
+++ src/config.ts
@@ -68,8 +68,8 @@ export const defaultXpacksCacheDirectory = env.get("NODE_LLAMA_CPP_XPACKS_CACHE_
     .asString();
 export const customCmakeOptionsEnvVarPrefix = "NODE_LLAMA_CPP_CMAKE_OPTION_";
 export const defaultChatSystemPrompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.\n" +
-    "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. " +
-    "If you don't know the answer to a question, please don't share false information.";
+    "If a question does not make any sense, or is not factually coherent, explain why instead of answering something incorrectly. " +
+    "If you don't know the answer to a question, don't share false information.";
 export const cliBinName = "node-llama-cpp";
 export const npxRunPrefix = "npx --no ";
 
