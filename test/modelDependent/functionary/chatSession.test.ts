f93b53cb7454b5bfa09b441d78265b6320a01bfd
Gilad S <giladgd@gmail.com>
2024-03-13 17:54:23 +0200

    fix: `DisposedError` was thrown when calling `.dispose()`

---
+++ test/modelDependent/functionary/chatSession.test.ts
@@ -0,0 +1,41 @@
+import {describe, expect, test} from "vitest";
+import {LlamaChatSession} from "../../../src/index.js";
+import {getModelFile} from "../../utils/modelFiles.js";
+import {getTestLlama} from "../../utils/getTestLlama.js";
+
+describe("functionary", () => {
+    describe("chat session", () => {
+        test("restore chat history", async () => {
+            const modelPath = await getModelFile("functionary-small-v2.2.q4_0.gguf");
+            const llama = await getTestLlama();
+
+            const model = await llama.loadModel({
+                modelPath
+            });
+            const context = await model.createContext({
+                contextSize: 4096
+            });
+            const chatSession = new LlamaChatSession({
+                contextSequence: context.getSequence()
+            });
+
+            const res = await chatSession.prompt("How much is 6+6");
+
+            expect(res).to.eql("6+6 equals 12.");
+
+            const chatHistory = chatSession.getChatHistory();
+
+            chatSession.dispose();
+            const chatSession2 = new LlamaChatSession({
+                contextSequence: context.getSequence()
+            });
+            chatSession2.setChatHistory(chatHistory);
+
+            const res2 = await chatSession2.prompt("Repeat your answer");
+
+            expect(res2).to.eql("6+6 equals 12.");
+        }, {
+            timeout: 1000 * 60 * 60 * 2
+        });
+    });
+});
