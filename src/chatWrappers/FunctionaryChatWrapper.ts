aa819af47a6ac2579ef0287ac44a11a2d08336c4
Gilad S <giladgd@gmail.com>
2024-04-24 02:37:57 +0300

    fix: `FunctionaryChatWrapper` bugs

--- src/chatWrappers/FunctionaryChatWrapper.ts
+++ src/chatWrappers/FunctionaryChatWrapper.ts
@@ -1,7 +1,7 @@
 import {ChatWrapper} from "../ChatWrapper.js";
 import {ChatHistoryItem, ChatModelFunctions, isChatModelResponseFunctionCall} from "../types.js";
 import {SpecialToken, LlamaText, SpecialTokensText} from "../utils/LlamaText.js";
-import {getTypeScriptTypeStringForGbnfJsonSchema} from "../utils/getTypeScriptTypeStringForGbnfJsonSchema.js";
+import {ChatModelFunctionsDocumentationGenerator} from "./utils/ChatModelFunctionsDocumentationGenerator.js";
 
 // source: https://github.com/MeetKai/functionary/blob/main/tests/prompt_test_v2.txt
 export class FunctionaryChatWrapper extends ChatWrapper {
@@ -101,7 +101,11 @@ export class FunctionaryChatWrapper extends ChatWrapper {
                             else if (isChatModelResponseFunctionCall(response)) {
                                 return LlamaText([
                                     response.raw != null
-                                        ? LlamaText(response.raw)
+                                        ? LlamaText(
+                                            response.raw.endsWith("\n")
+                                                ? response.raw.slice(0, -"\n".length)
+                                                : response.raw
+                                        )
                                         : LlamaText([
                                             (isFirstItem && isFirstResponse)
                                                 ? LlamaText([])
@@ -124,14 +128,19 @@ export class FunctionaryChatWrapper extends ChatWrapper {
                                                 : JSON.stringify(response.result)
                                         ]),
 
-                                    hasFunctions
-                                        ? LlamaText([])
-                                        : LlamaText([
-                                            new SpecialTokensText("\n"),
-                                            new SpecialTokensText("<|from|>assistant\n"),
-                                            new SpecialTokensText("<|recipient|>all\n"),
-                                            new SpecialTokensText("<|content|>")
-                                        ])
+                                    (isLastResponse && isLastItem)
+                                        ? hasFunctions
+                                            ? LlamaText([
+                                                new SpecialTokensText("\n"),
+                                                new SpecialTokensText("<|from|>assistant\n")
+                                            ])
+                                            : LlamaText([
+                                                new SpecialTokensText("\n"),
+                                                new SpecialTokensText("<|from|>assistant\n"),
+                                                new SpecialTokensText("<|recipient|>all\n"),
+                                                new SpecialTokensText("<|content|>")
+                                            ])
+                                        : LlamaText([])
                                 ]);
                             }
 
@@ -167,6 +176,16 @@ export class FunctionaryChatWrapper extends ChatWrapper {
             };
         }
 
+        const textResponseStart = [
+            " \n",
+            " \n\n",
+            "\n",
+            "n\n"
+        ].flatMap((prefix) => [
+            LlamaText(prefix + "<|from|>assistant\n<|recipient|>all\n<|content|>"),
+            LlamaText(new SpecialTokensText(prefix + "<|from|>assistant\n<|recipient|>all\n<|content|>"))
+        ]);
+
         return {
             contextText,
             stopGenerationTriggers: [
@@ -181,20 +200,10 @@ export class FunctionaryChatWrapper extends ChatWrapper {
                 LlamaText(new SpecialTokensText("<|stop|>")),
                 LlamaText(new SpecialTokensText("\n<|from|>user"))
             ],
-            ignoreStartText: [
-                LlamaText("\n<|from|>assistant\n<|recipient|>all\n<|content|>"),
-                LlamaText(new SpecialTokensText("\n<|from|>assistant\n<|recipient|>all\n<|content|>")),
-                LlamaText("\n\n<|from|>assistant\n<|recipient|>all\n<|content|>"),
-                LlamaText(new SpecialTokensText("\n\n<|from|>assistant\n<|recipient|>all\n<|content|>"))
-            ],
+            ignoreStartText: textResponseStart,
             functionCall: {
                 initiallyEngaged: true,
-                disengageInitiallyEngaged: [
-                    LlamaText("\n<|from|>assistant\n<|recipient|>all\n<|content|>"),
-                    LlamaText(new SpecialTokensText("\n<|from|>assistant\n<|recipient|>all\n<|content|>")),
-                    LlamaText("\n\n<|from|>assistant\n<|recipient|>all\n<|content|>"),
-                    LlamaText(new SpecialTokensText("\n\n<|from|>assistant\n<|recipient|>all\n<|content|>"))
-                ]
+                disengageInitiallyEngaged: textResponseStart
             }
         };
     }
@@ -202,35 +211,24 @@ export class FunctionaryChatWrapper extends ChatWrapper {
     public override generateAvailableFunctionsSystemText(availableFunctions: ChatModelFunctions, {documentParams = true}: {
         documentParams?: boolean
     }) {
+        const functionsDocumentationGenerator = new ChatModelFunctionsDocumentationGenerator(availableFunctions);
+
+        if (!functionsDocumentationGenerator.hasAnyFunctions)
+            return "";
+
         const availableFunctionNames = Object.keys(availableFunctions ?? {});
 
         if (availableFunctionNames.length === 0)
             return "";
 
-        return "// Supported function definitions that should be called when necessary.\n" +
-            "namespace functions {\n\n" +
-            availableFunctionNames
-                .map((functionName) => {
-                    if (functionName === "all")
-                        throw new Error('Function name "all" is reserved and cannot be used');
-
-                    const functionDefinition = availableFunctions[functionName];
-                    let res = "";
-
-                    if (functionDefinition?.description != null && functionDefinition.description.trim() !== "")
-                        res += "// " + functionDefinition.description.split("\n").join("\n// ") + "\n";
-
-                    res += "type " + functionName + " = (";
-
-                    if (documentParams && functionDefinition?.params != null)
-                        res += "_: " + getTypeScriptTypeStringForGbnfJsonSchema(functionDefinition.params);
-
-                    res += ") => any;";
-
-                    return res;
-                })
-                .join("\n\n") +
-            "\n\n} // namespace functions";
+        return [
+            "// Supported function definitions that should be called when necessary.",
+            "namespace functions {",
+            "",
+            functionsDocumentationGenerator.getTypeScriptFunctionTypes({documentParams, reservedFunctionNames: ["all"]}),
+            "",
+            "} // namespace functions"
+        ].join("\n");
     }
 
     public override addAvailableFunctionsSystemMessageToHistory(
