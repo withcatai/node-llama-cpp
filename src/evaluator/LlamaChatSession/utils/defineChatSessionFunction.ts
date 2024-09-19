c7f283ec7ab497beda2ba556e2a4a93bbd5c8974
Gilad S <giladgd@gmail.com>
2024-09-20 02:45:03 +0300

    fix: improve `defineChatSessionFunction`

--- src/evaluator/LlamaChatSession/utils/defineChatSessionFunction.ts
+++ src/evaluator/LlamaChatSession/utils/defineChatSessionFunction.ts
@@ -5,6 +5,8 @@ import {ChatSessionModelFunction} from "../../../types.js";
  * Define a function that can be used by the model in a chat session, and return it.
  *
  * This is a helper function to facilitate defining functions with full TypeScript type information.
+ *
+ * The handler function can return a Promise, and the return value will be awaited before being returned to the model.
  * @param functionDefinition
  */
 export function defineChatSessionFunction<const Params extends GbnfJsonSchema | undefined>({
@@ -13,8 +15,8 @@ export function defineChatSessionFunction<const Params extends GbnfJsonSchema | 
     handler
 }: {
     description?: string,
-    params?: Params,
-    handler: (params: GbnfJsonSchemaToType<Params>) => any
+    params?: Params & GbnfJsonSchema,
+    handler: (params: GbnfJsonSchemaToType<Params>) => Promise<any> | any
 }): ChatSessionModelFunction<Params> {
     return {
         description,
