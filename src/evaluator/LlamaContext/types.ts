2c1af7abcab0b8e9b9c144bacba31474e03e7689
Gilad S <giladgd@gmail.com>
2024-12-25 03:30:04 +0200

    fix: typos

--- src/evaluator/LlamaContext/types.ts
+++ src/evaluator/LlamaContext/types.ts
@@ -209,7 +209,7 @@ export type LlamaContextSequenceRepeatPenalty = {
 export type BatchingOptions = {
     /**
      * The strategy used to dispatch items to be processed when there are items pending to be processed.
-     * - **`"nextTick"`** - dispatch the items on the next even loop tick.
+     * - **`"nextTick"`** - dispatch the items on the next event loop tick.
      * You can provide a custom function to define a custom dispatch schedule.
      *
      * Defaults to `"nextTick"`.
