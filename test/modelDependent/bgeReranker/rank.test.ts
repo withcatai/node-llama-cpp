55f5b26ca24da2fca5bbcdb24d69a9f445b03798
Gilad S <giladgd@gmail.com>
2025-01-07 00:25:42 +0200

    test: fix tests

--- test/modelDependent/bgeReranker/rank.test.ts
+++ test/modelDependent/bgeReranker/rank.test.ts
@@ -124,7 +124,6 @@ describe("bgeReranker", () => {
                 "I love eating pizza with extra cheese",
                 "Dogs love to play fetch with their owners",
                 "The capital of France is Paris",
-                "Drinking water is important for staying hydrated",
                 "Mount Everest is the tallest mountain in the world",
                 "A warm cup of tea is perfect for a cold winter day",
                 "Not all the things that shine are made of gold",
@@ -169,10 +168,6 @@ describe("bgeReranker", () => {
                 },
                 {
                   "document": "The sky is clear and blue today",
-                  "score": -11,
-                },
-                {
-                  "document": "Drinking water is important for staying hydrated",
                   "score": -11,
                 },
                 {
