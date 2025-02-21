642da5bb98e256e06be0ebb653f0de267cd0dabf
Gilad S <giladgd@gmail.com>
2025-02-21 18:28:06 +0200

    chore(Electron template): update links

--- templates/electron-typescript-react/src/App/App.tsx
+++ templates/electron-typescript-react/src/App/App.tsx
@@ -139,22 +139,44 @@ export function App() {
                                     Star <code>node-llama-cpp</code> on GitHub
                                 </div>
                             </a>
+
+                            <div className="separator"></div>
+                            <div className="title">DeepSeek R1 Distill Qwen model</div>
                             <div className="links">
                                 <a
                                     target="_blank"
-                                    href="https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-14B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-14B-Q4_K_M.gguf"
+                                    href="https://huggingface.co/mradermacher/DeepSeek-R1-Distill-Qwen-7B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-7B.Q4_K_M.gguf"
                                 >
                                     <DownloadIconSVG className="downloadIcon" />
-                                    <div className="text">Get DeepSeek R1 Distill Qwen 14B model</div>
+                                    <div className="text">Get 7B</div>
+                                </a>
+                                <div className="separator" />
+                                <a
+                                    target="_blank"
+                                    href="https://huggingface.co/mradermacher/DeepSeek-R1-Distill-Qwen-14B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-14B.Q4_K_M.gguf"
+                                >
+                                    <DownloadIconSVG className="downloadIcon" />
+                                    <div className="text">Get 14B</div>
+                                </a>
+                                <div className="separator" />
+                                <a
+                                    target="_blank"
+                                    href="https://huggingface.co/mradermacher/DeepSeek-R1-Distill-Qwen-32B-GGUF/resolve/main/DeepSeek-R1-Distill-Qwen-32B.Q4_K_M.gguf"
+                                >
+                                    <DownloadIconSVG className="downloadIcon" />
+                                    <div className="text">Get 32B</div>
                                 </a>
                             </div>
+
+                            <div className="separator"></div>
+                            <div className="title">Other models</div>
                             <div className="links">
                                 <a
                                     target="_blank"
                                     href="https://huggingface.co/mradermacher/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct.Q4_K_M.gguf"
                                 >
                                     <DownloadIconSVG className="downloadIcon" />
-                                    <div className="text">Get Llama 3.1 8B model</div>
+                                    <div className="text">Get Llama 3.1 8B</div>
                                 </a>
                                 <div className="separator" />
                                 <a
@@ -162,10 +184,12 @@ export function App() {
                                     href="https://huggingface.co/bartowski/gemma-2-2b-it-GGUF/resolve/main/gemma-2-2b-it-Q4_K_M.gguf"
                                 >
                                     <DownloadIconSVG className="downloadIcon" />
-                                    <div className="text">Get Gemma 2 2B model</div>
+                                    <div className="text">Get Gemma 2 2B</div>
                                 </a>
                             </div>
-                            <a className="browseLink" target="_blank" href="https://huggingface.co/mradermacher">
+
+                            <div className="separator"></div>
+                            <a className="browseLink" target="_blank" href="https://huggingface.co/models?pipeline_tag=text-generation&library=gguf&sort=trending">
                                 <SearchIconSVG className="searchIcon" />
                                 <div className="text">Find more models</div>
                             </a>
