f0e7f0ac99c02ef96168e22e3e7e71756c72bdd5
Gilad S <giladgd@gmail.com>
2025-03-22 04:47:57 +0200

    build: dispose the llama instance before a test worker exists

--- test/utils/getTestLlama.ts
+++ test/utils/getTestLlama.ts
@@ -1,6 +1,19 @@
+import {afterAll} from "vitest";
 import {getLlama, Llama} from "../../src/index.js";
+import {isCI} from "../../src/config.js";
 
 let llamaPromise: Promise<Llama> | null = null;
+
+afterAll(async () => {
+    if (!isCI)
+        return;
+
+    if (llamaPromise != null) {
+        const temp = llamaPromise;
+        llamaPromise = null;
+        await (await temp).dispose();
+    }
+});
 
 export async function getTestLlama() {
     if (llamaPromise != null)
