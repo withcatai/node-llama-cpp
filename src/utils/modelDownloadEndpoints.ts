96c78dab3576efd095da52694a90f4f8a13d4240
Gilad S <giladgd@gmail.com>
2025-05-12 02:56:29 +0300

    docs: document new env vars

--- src/utils/modelDownloadEndpoints.ts
+++ src/utils/modelDownloadEndpoints.ts
@@ -4,6 +4,8 @@ export type ModelDownloadEndpoints = {
     /**
      * Endpoint to use for resolving Hugging Face model URIs (`hf:`).
      *
+     * Can also be set by using the environment variable `HF_ENDPOINT` or `MODEL_ENDPOINT`.
+     *
      * Defaults to `"https://huggingface.co/"`.
      * @see [Model URIs](https://node-llama-cpp.withcat.ai/guide/downloading-models#model-uris)
      */
