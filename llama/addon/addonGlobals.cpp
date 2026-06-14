2ad108a51c203f844cb44c2cc979f42d8f050228
Gilad S. <giladgd@gmail.com>
2026-06-14 18:28:18 +0200

    fix: don't crash when loading huge models

--- llama/addon/addonGlobals.cpp
+++ llama/addon/addonGlobals.cpp
@@ -4,7 +4,7 @@
 #include "napi.h"
 
 void adjustNapiExternalMemoryAdd(Napi::Env env, uint64_t size) {
-    const uint64_t chunkSize = std::numeric_limits<int64_t>::max();
+    constexpr uint64_t chunkSize = std::min(512LL * 1024LL * 1024LL, std::numeric_limits<int64_t>::max());
     while (size > 0) {
         int64_t adjustSize = std::min(size, chunkSize);
         Napi::MemoryManagement::AdjustExternalMemory(env, adjustSize);
@@ -13,7 +13,7 @@ void adjustNapiExternalMemoryAdd(Napi::Env env, uint64_t size) {
 }
 
 void adjustNapiExternalMemorySubtract(Napi::Env env, uint64_t size) {
-    const uint64_t chunkSize = std::numeric_limits<int64_t>::max();
+    constexpr uint64_t chunkSize = std::min(512LL * 1024LL * 1024LL, std::numeric_limits<int64_t>::max());
     while (size > 0) {
         int64_t adjustSize = std::min(size, chunkSize);
         Napi::MemoryManagement::AdjustExternalMemory(env, -adjustSize);
