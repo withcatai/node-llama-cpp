da96bd263060d02cb7be215bd40ebd29bc9cac18
Gilad S. <giladgd@gmail.com>
2026-09-23 19:37:44 +0200

    fix(Electron template): improve react hook example

--- templates/electron-typescript-react/src/hooks/useExternalState.ts
+++ templates/electron-typescript-react/src/hooks/useExternalState.ts
@@ -1,24 +1,13 @@
-import {useEffect, useState} from "react";
+import {useSyncExternalStore} from "react";
 import {State} from "lifecycle-utils";
 
 export function useExternalState<const StateType, const R>(state: State<StateType>, selector: ((state: StateType) => R)): R;
 export function useExternalState<const StateType>(state: State<StateType>): StateType;
 export function useExternalState<const StateType>(state: State<StateType>, selector?: ((state: StateType) => any) | null): StateType {
-    const [currentState, setCurrentState] = useState(() => (
-        selector == null
-            ? state.state
-            : selector(state.state)
-    ));
-
-    useEffect(() => {
-        return state.createChangeListener((newState) => {
-            setCurrentState(
-                selector == null
-                    ? newState
-                    : selector(newState)
-            );
-        }, true).dispose;
-    }, [state]);
+    const currentState = useSyncExternalStore(
+        (callback) => state.createChangeListener(callback).dispose,
+        () => (selector == null ? state.state : selector(state.state))
+    );
 
     return currentState;
 }
